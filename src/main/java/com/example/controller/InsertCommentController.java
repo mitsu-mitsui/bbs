f6c797bc1dfc90c125f99939259d15b1de4cae4f
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-07-09 15:05:55 +0900

    コントローラーのアノテーション修正

--- src/main/java/com/example/controller/InsertCommentController.java
+++ src/main/java/com/example/controller/InsertCommentController.java
@@ -2,12 +2,15 @@ package com.example.controller;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Comment;
 import com.example.form.CommentForm;
 import com.example.repository.CommentRepository;
 
+@Controller
+@RequestMapping("")
 public class InsertCommentController {
 	@Autowired
 	private CommentRepository commentRepository;
