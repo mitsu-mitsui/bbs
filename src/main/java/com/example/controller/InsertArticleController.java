3e0633f076c0dedc64a67d267b80bc850e0516cf
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-07-09 15:03:48 +0900

    表示画面のコントローラーを記事投稿のコントローラーに編入

--- src/main/java/com/example/controller/InsertArticleController.java
+++ src/main/java/com/example/controller/InsertArticleController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.BeanUtils;
@@ -8,10 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
 import com.example.form.ArticleForm;
+import com.example.form.CommentForm;
 import com.example.repository.ArticleRepository;
 
 /**
@@ -29,6 +33,32 @@ public class InsertArticleController {
 	@Autowired
 	private HttpSession session;
 
+	@ModelAttribute
+	private ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
+
+	@ModelAttribute
+	private CommentForm setUpCommentForm() {
+		return new CommentForm();
+	}
+
+	/**
+	 * 記事一覧を表示.
+	 * 
+	 * @param model リクエストスコープ
+	 * @return 記事一覧ページ
+	 */
+	@RequestMapping("")
+	public String index(Model model) {
+		List<Article> articleList = articleRepository.findAll();
+		if (articleList.size() == 0) {
+			model.addAttribute("noArticle", "記事がありません　投稿してね");
+		}
+		model.addAttribute("articleList", articleList);
+		return "bbs";
+	}
+
 	/**
 	 * 記事を投稿.
 	 * 
@@ -39,7 +69,7 @@ public class InsertArticleController {
 	public String insertArticle(@Validated ArticleForm articleForm, BindingResult result, Model model) {
 		session.setAttribute("name", articleForm.getName());
 		if (result.hasErrors()) {
-			return "forward:/";
+			return index(model);
 		}
 		Article article = new Article();
 		BeanUtils.copyProperties(articleForm, article);
